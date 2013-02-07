3372d7fe73cf9b0cd7619d8b001f68bf018b4192
ukqida@gmail.com <ukqida@gmail.com@8dec5abe-996e-0da9-f060-d0ef34c479f2>
2013-02-07 15:26:24 +0000

    Initial import

---
+++ trunk/srm-mzquantml-convertor/src/main/java/uk/ac/liv/srmconvertor/SrmConvertor.java
@@ -0,0 +1,17 @@
+
+package uk.ac.liv.srmconvertor;
+
+import uk.ac.liv.jmzqml.model.mzqml.MzQuantML;
+
+/**
+ * Hello world!
+ *
+ */
+public class SrmConvertor {
+
+    public static void convert(String inputFn, String outputFn) {
+        
+        MzQuantML mzq = new MzQuantML();
+    }
+
+}
